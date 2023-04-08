8ff4072c4a9da16447500b7ce53e786de55c1f30
TurtleCat02 <bcspenguin1@gmail.com>
2023-04-08 14:03:37 -0400

    Playing around with reflection

--- src/main/java/edu/wpi/teamR/mapdb/Edge.java
+++ src/main/java/edu/wpi/teamR/mapdb/Edge.java
@@ -1,16 +1,18 @@
 package edu.wpi.teamR.mapdb;
 
+import edu.wpi.teamR.csv.MapData;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter(AccessLevel.PACKAGE)
-public class Edge {
+public class Edge extends MapData {
     private int startNode, endNode;
 
     Edge(int startNode, int endNode){
         this.startNode = startNode;
         this.endNode = endNode;
     }
+
 }
