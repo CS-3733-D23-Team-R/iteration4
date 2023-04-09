a03a8fac603d08cce539f1bccc676b96882880e2
john-robinson-git <john.robinson.engineering@gmail.com>
2023-04-09 17:42:59 -0400

    added stubs for all functions in the RequestDatabase facade and created all entities, have not fully implemented requestType

---
+++ src/main/java/edu/wpi/teamR/requestdb/ItemRequest.java
@@ -0,0 +1,31 @@
+package edu.wpi.teamR.requestdb;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter(AccessLevel.PACKAGE)
+abstract class ItemRequest {
+
+    private int requestID;
+    private String requesterName;
+    private String location;
+    private String staffMember;
+    private String additionalNotes;
+    private java.sql.Timestamp requestDate;
+    private RequestStatus requestStatus;
+
+    public ItemRequest(int requestID, String requesterName, String location, String staffMember, String additionalNotes, Timestamp requestDate, RequestStatus requestStatus) {
+        this.requestID = requestID;
+        this.requesterName = requesterName;
+        this.location = location;
+        this.staffMember = staffMember;
+        this.additionalNotes = additionalNotes;
+        this.requestDate = requestDate;
+        this.requestStatus = requestStatus;
+    }
+
+}
