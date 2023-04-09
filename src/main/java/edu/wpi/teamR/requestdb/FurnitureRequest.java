4533fc9b5c50f7fab539842906e06dc766632d3e
john-robinson-git <john.robinson.engineering@gmail.com>
2023-04-09 18:18:50 -0400

    added mealTypw and furnitureType

--- src/main/java/edu/wpi/teamR/requestdb/FurnitureRequest.java
+++ src/main/java/edu/wpi/teamR/requestdb/FurnitureRequest.java
@@ -1,9 +1,17 @@
 package edu.wpi.teamR.requestdb;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.Timestamp;
 
+@Getter
+@Setter(AccessLevel.PACKAGE)
 public class FurnitureRequest extends ItemRequest{
-    public FurnitureRequest(int requestID, String requestorName, String location, String staffMember, String additionalNotes, Timestamp requestDate, RequestStatus requestStatus) {
+    private String furnitureType;
+    public FurnitureRequest(int requestID, String requestorName, String location, String staffMember, String additionalNotes, Timestamp requestDate, RequestStatus requestStatus, String furnitureType) {
         super(requestID, requestorName, location, staffMember, additionalNotes, requestDate, requestStatus);
+        this.furnitureType = furnitureType;
     }
 }
