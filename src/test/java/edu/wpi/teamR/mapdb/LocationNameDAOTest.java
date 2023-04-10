37d5efda2d6c923f56d6f480d9305dec4942e3a9
altonlm <121144911+altonlm@users.noreply.github.com>
2023-04-10 11:21:42 -0400

    pretty much finished tests for Move and LocationName DAOs

--- src/test/java/edu/wpi/teamR/mapdb/LocationNameDAOTest.java
+++ src/test/java/edu/wpi/teamR/mapdb/LocationNameDAOTest.java
@@ -1,11 +1,14 @@
 package edu.wpi.teamR.mapdb;
 
 import edu.wpi.teamR.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,32 +18,155 @@ class LocationNameDAOTest {
     private Connection connection;
     @BeforeAll
     void starterFunction() throws SQLException, ClassNotFoundException {
+        Configuration.changeSchemaName("prototype1test");
         connection = Configuration.getConnection();
         locationNameDAO = new LocationNameDAO(connection);
     }
 
-    @Test
-    void getLocations() {
+    @BeforeEach
+    void deleteOldData() throws SQLException {
+        locationNameDAO.deleteAllLocationNames();
+    }
 
+    @AfterAll
+    void clearDataDeleteConnection() throws SQLException {
+        locationNameDAO.deleteAllLocationNames();
+        connection.close();
     }
 
     @Test
-    void getLocationByNodeID() {
+    void getLocations() throws SQLException {
+        ArrayList<LocationName> locationNames;
+        LocationName locationName;
+
+        locationNames = locationNameDAO.getLocations();
+        assertEquals(locationNames.size(), 0);
+
+        locationNameDAO.addLocationName("Location1", "l1", "node");
+
+        locationNames = locationNameDAO.getLocations();
+        assertEquals(locationNames.size(), 1);
+        locationName = locationNames.get(0);
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getShortName(), "l1");
+        assertEquals(locationName.getNodeType(), "node");
+
+        locationNameDAO.addLocationName("Location2", "l2", "node");
+
+        locationNames = locationNameDAO.getLocations();
+        assertEquals(locationNames.size(), 2);
+
+        locationNameDAO.addLocationName("Location3", "l2", "node");
+
+        locationNames = locationNameDAO.getLocations();
+        assertEquals(locationNames.size(), 3);
     }
 
     @Test
-    void getLocationsByNodeType() {
+    void getLocationsByNodeType() throws SQLException {
+        ArrayList<LocationName> locationNames;
+        LocationName locationName;
+
+        locationNames = locationNameDAO.getLocationsByNodeType("test");
+        assertEquals(locationNames.size(), 0);
+
+        locationNameDAO.addLocationName("Location1", "l1", "node");
+        locationNameDAO.addLocationName("Location2", "l2", "node");
+        locationNameDAO.addLocationName("Location3", "l3", "diff");
+
+        locationNames = locationNameDAO.getLocationsByNodeType("node");
+        assertEquals(locationNames.size(), 2);
+
+        locationNames = locationNameDAO.getLocationsByNodeType("diff");
+        assertEquals(locationNames.size(), 1);
+
+        locationNames = locationNameDAO.getLocationsByNodeType("test");
+        assertEquals(locationNames.size(), 0);
     }
 
     @Test
-    void getLocationByLongName() {
+    void getLocationByLongName() throws SQLException {
+        LocationName locationName;
+
+        locationName = locationNameDAO.getLocationByLongName("test"); //TODO: ERROR CASE FOR MISSED SELECT
+
+        locationNameDAO.addLocationName("Location1", "l1", "node");
+        locationNameDAO.addLocationName("Location2", "l2", "node");
+        locationNameDAO.addLocationName("Location3", "l3", "diff");
+
+        locationName = locationNameDAO.getLocationByLongName("Location1");
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getLongName(), "l1");
+        assertEquals(locationName.getLongName(), "node");
+
+        locationName = locationNameDAO.getLocationByLongName("Location2");
+        assertEquals(locationName.getLongName(), "Location2");
+        assertEquals(locationName.getLongName(), "l2");
+        assertEquals(locationName.getLongName(), "node");
+
+        locationName = locationNameDAO.getLocationByLongName("Location3");
+        assertEquals(locationName.getLongName(), "Location3");
+        assertEquals(locationName.getLongName(), "l3");
+        assertEquals(locationName.getLongName(), "diff");
+
+        locationName = locationNameDAO.getLocationByLongName("test");
     }
 
     @Test
-    void modifyLocationNameType() {
+    void modifyLocationNameType() throws SQLException {
+        LocationName locationName;
+
+        locationNameDAO.addLocationName("Location1", "l1", "node");
+        locationNameDAO.addLocationName("Location2", "l2", "node");
+        locationNameDAO.addLocationName("Location3", "l3", "diff");
+
+        locationName = locationNameDAO.getLocationByLongName("Location1");
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getLongName(), "l1");
+        assertEquals(locationName.getLongName(), "node");
+
+        locationNameDAO.modifyLocationNameType("Location1", "test");
+
+        locationName = locationNameDAO.getLocationByLongName("Location1");
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getLongName(), "l1");
+        assertEquals(locationName.getLongName(), "test");
+
+        //Ensure everything else is unchanged
+        assertEquals(locationName.getLongName(), "Location2");
+        assertEquals(locationName.getLongName(), "l2");
+        assertEquals(locationName.getLongName(), "node");
+        assertEquals(locationName.getLongName(), "Location3");
+        assertEquals(locationName.getLongName(), "l3");
+        assertEquals(locationName.getLongName(), "diff");
     }
 
     @Test
-    void modifyLocationNameShortName() {
+    void modifyLocationNameShortName() throws SQLException {
+        LocationName locationName;
+
+        locationNameDAO.addLocationName("Location1", "l1", "node");
+        locationNameDAO.addLocationName("Location2", "l2", "node");
+        locationNameDAO.addLocationName("Location3", "l3", "diff");
+
+        locationName = locationNameDAO.getLocationByLongName("Location1");
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getLongName(), "l1");
+        assertEquals(locationName.getLongName(), "node");
+
+        locationNameDAO.modifyLocationNameShortName("Location1", "testShortName");
+
+        locationName = locationNameDAO.getLocationByLongName("Location1");
+        assertEquals(locationName.getLongName(), "Location1");
+        assertEquals(locationName.getLongName(), "l1");
+        assertEquals(locationName.getLongName(), "testShortName");
+
+        //Ensure everything else is unchanged
+        assertEquals(locationName.getLongName(), "Location2");
+        assertEquals(locationName.getLongName(), "l2");
+        assertEquals(locationName.getLongName(), "node");
+        assertEquals(locationName.getLongName(), "Location3");
+        assertEquals(locationName.getLongName(), "l3");
+        assertEquals(locationName.getLongName(), "diff");
     }
 }
--- src/test/java/edu/wpi/teamR/mapdb/MoveDAOTest.java
+++ src/test/java/edu/wpi/teamR/mapdb/MoveDAOTest.java
@@ -1,31 +1,190 @@
 package edu.wpi.teamR.mapdb;
 
+import edu.wpi.teamR.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MoveDAOTest {
 
+    private Connection connection;
+    private LocationNameDAO locationNameDAO;
+    private NodeDAO nodeDAO;
+    private MoveDAO moveDAO;
+
+    @BeforeAll
+    void starterFunction() throws SQLException, ClassNotFoundException {
+        Configuration.changeSchemaName("prototype1test");
+        connection = Configuration.getConnection();
+        locationNameDAO = new LocationNameDAO(connection);
+        nodeDAO = new NodeDAO(connection);
+        moveDAO = new MoveDAO(connection);
+    }
+
+    @BeforeEach
+    void deleteOldData() throws SQLException {
+        moveDAO.deleteAllMoves();
+        nodeDAO.deleteAllNodes();
+        locationNameDAO.deleteAllLocationNames();
+    }
+
+    @AfterAll
+    void clearDataDeleteConnection() throws SQLException {
+        moveDAO.deleteAllMoves();
+        nodeDAO.deleteAllNodes();
+        locationNameDAO.deleteAllLocationNames();
+        connection.close();
+    }
+
 
     @Test
-    void getMoves() {
+    void getMoves() throws SQLException {
+        ArrayList<Move> moves;
+        Move move;
+        Node node;
+        Date date;
+
+        moves = moveDAO.getMoves();
+        assertEquals(moves.size(), 0);
+
+        node = nodeDAO.addNode(0, 0, "L1", "temp");
+        locationNameDAO.addLocationName("name1", "", "type");
+        date = Date.valueOf(LocalDate.now());
+        move = moveDAO.addMove(node.getNodeID(), "name1", date);
+
+        moves = moveDAO.getMoves();
+        assertEquals(moves.size(), 1);
+        move = moves.get(0);
+        assertEquals(move.getNodeID(), node.getNodeID());
+        assertEquals(move.getLongName(), "name1");
     }
 
     @Test
-    void getMovesByNodeID() {
+    void getMovesByNodeID() throws SQLException {
+        ArrayList<Move> moves;
+        Node node;
+        Date date;
+
+        node = nodeDAO.addNode(0, 0, "L1", "temp");
+        locationNameDAO.addLocationName("name1", "", "type");
+        date = Date.valueOf(LocalDate.now().minusDays(0));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after first move added
+        assertEquals(moves.size(), 1);
+
+        date = Date.valueOf(LocalDate.now().minusDays(1));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after 2nd move added
+        assertEquals(moves.size(), 2);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()+1); //invalid id //TODO: need to replace with error case
+        assertEquals(moves.size(), 0);
     }
 
+    /*
+    Think method isn't helpful
     @Test
     void getLatestMoveByNodeID() {
-    }
 
+    }
+     */
+
+    /*
+    Tested alongside others
     @Test
     void addMove() {
     }
+     */
 
     @Test
-    void deleteMovesByNode() {
+    void deleteMovesByNode() throws SQLException {
+        ArrayList<Move> moves;
+        Node node;
+        Node node2;
+        Date date;
+
+        node = nodeDAO.addNode(0, 0, "L1", "temp");
+        node2 = nodeDAO.addNode(0, 0, "L1", "temp");
+        locationNameDAO.addLocationName("name1", "", "type");
+        date = Date.valueOf(LocalDate.now().minusDays(0));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after first move added
+        assertEquals(moves.size(), 1);
+
+        moveDAO.deleteMovesByNode(node.getNodeID());
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after first move deleted
+        assertEquals(moves.size(), 0);
+
+
+        date = Date.valueOf(LocalDate.now().minusDays(0));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+        date = Date.valueOf(LocalDate.now().minusDays(1));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+        date = Date.valueOf(LocalDate.now().minusDays(1));
+        moveDAO.addMove(node2.getNodeID(), "name1", date);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after three moves added
+        assertEquals(moves.size(), 3);
+
+        moveDAO.deleteMovesByNode(node.getNodeID());
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after two moves deleted
+        assertEquals(moves.size(), 1);
+
+        moveDAO.deleteMovesByNode(node2.getNodeID());
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after last move deleted
+        assertEquals(moves.size(), 1);
     }
 
     @Test
-    void deleteMovesByLongName() {
+    void deleteMovesByLongName() throws SQLException {
+        ArrayList<Move> moves;
+        Node node;
+        Date date;
+
+        node = nodeDAO.addNode(0, 0, "L1", "temp");
+        locationNameDAO.addLocationName("name1", "", "type");
+        locationNameDAO.addLocationName("name2", "", "type");
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //before anything added
+        assertEquals(moves.size(), 0);
+
+        date = Date.valueOf(LocalDate.now().minusDays(0));
+        moveDAO.addMove(node.getNodeID(), "name1", date);
+        date = Date.valueOf(LocalDate.now().minusDays(1));
+        moveDAO.addMove(node.getNodeID(), "name2", date);
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after both added
+        assertEquals(moves.size(), 2);
+
+        moveDAO.deleteMovesByLongName("name1");
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after one deleted
+        assertEquals(moves.size(), 1);
+
+        moveDAO.deleteMovesByLongName("fake");
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after fake delete
+        assertEquals(moves.size(), 1);
+
+        moveDAO.deleteMovesByLongName("node2");
+
+        moves = moveDAO.getMovesByNodeID(node.getNodeID()); //after last one deleted
+        assertEquals(moves.size(), 0);
     }
 }
