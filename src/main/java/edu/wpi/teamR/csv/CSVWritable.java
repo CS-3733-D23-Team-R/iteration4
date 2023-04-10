9755e7b8ff8808e52a97f7f60968689304dccbf2
TurtleCat02 <bcspenguin1@gmail.com>
2023-04-09 20:57:57 -0400

    Changed docstring

--- src/main/java/edu/wpi/teamR/csv/CSVWritable.java
+++ src/main/java/edu/wpi/teamR/csv/CSVWritable.java
@@ -3,7 +3,7 @@ package edu.wpi.teamR.csv;
 public interface CSVWritable {
     /**
      *
-     * @return String with each of the fields to write, in order that they appear in the constructor
+     * @return String with each of the constructor values to write, in order that they appear in the constructor
      */
     String toCSVEntry();
     String getCSVColumns();
