898026e665261c48083471a828ae117c1738eae4
nathanielitty <nitty@wpi.edu>
2023-04-10 01:00:11 -0400

    Created sidebar with button functionality

--- src/main/java/edu/wpi/teamR/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamR/controllers/HomeController.java
@@ -2,11 +2,14 @@ package edu.wpi.teamR.controllers;
 
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.io.IOException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.*;
 import org.controlsfx.control.PopOver;
 
