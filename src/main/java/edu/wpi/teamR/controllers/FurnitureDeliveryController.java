f269e73b4aca0ee6c5f6681189b6526d32d9bdd0
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-04-11 20:30:11 -0400

    Updated Meal&Furniture Controller
    Waiting for DAOs to fix the errors

--- src/main/java/edu/wpi/teamR/controllers/FurnitureDeliveryController.java
+++ src/main/java/edu/wpi/teamR/controllers/FurnitureDeliveryController.java
@@ -2,11 +2,19 @@ package edu.wpi.teamR.controllers;
 
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
+import edu.wpi.teamR.requestdb.FurnitureRequest;
+import edu.wpi.teamR.requestdb.MealRequest;
+import edu.wpi.teamR.requestdb.RequestStatus;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import org.controlsfx.control.SearchableComboBox;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class FurnitureDeliveryController {
 
@@ -21,7 +29,64 @@ public class FurnitureDeliveryController {
 
     @FXML public void initialize() {
         cancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+        resetButton.setOnMouseClicked(event -> clear());
+        submitButton.setOnMouseClicked(event -> submit());
+
+        furnitureTypeField.setValue("Select Furniture");
+        locationField.setValue("Select Your Location");
     }
 
+    public Timestamp CurrentDateTime(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        return new Timestamp(now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now.getHour(),now.getMinute(),now.getSecond(),now.getNano());
+    }
+
+    @FXML
+    public void clear() {
+        nameField.clear();
+        staffMemberField.clear();
+        notesField.clear();
+
+        furnitureTypeField.setValue("Select Furniture");
+        locationField.setValue("Select Your Location");
+    }
+
+    @FXML
+    public void submit() {
+        String furnitureType = furnitureTypeField.getValue().toString();
+        String location = locationField.getValue().toString();
+
+        if (furnitureType == "Select Furniture") {
+            furnitureType = "";
+        }
+
+        if (location == "Select Your Location") {
+            location = "";
+        }
+
+        Navigation.navigate(Screen.HOME);
+        int id = 0;
+        ArrayList<MealRequest> foodList = MealRequestDAO.getInstance().getFoodRequests();
+        for(MealRequest foodRequest: foodList){
+            if(id < foodRequest.getRequestID()){
+                id = foodRequest.getRequestID();
+            }
+        }
+        ArrayList<FurnitureRequest> furnList = FurnitureRequestDAO.getInstance().getFurnitureRequests();
+        for(FurnitureRequest furnitureRequest: furnList){
+            if(id < furnitureRequest.getRequestID()){
+                id = furnitureRequest.getRequestID();
+            }
+        }
+        id++;
+        try {
+            FurnitureRequestDAO.getInstance().addFurnitureRequest(id, nameField.getText(), location, furnitureType, staffMemberField.getText(), notesField.getText(), CurrentDateTime(), RequestStatus.Unstarted);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/edu/wpi/teamR/controllers/MealDeliveryController.java
+++ src/main/java/edu/wpi/teamR/controllers/MealDeliveryController.java
@@ -2,10 +2,18 @@ package edu.wpi.teamR.controllers;
 
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
+import edu.wpi.teamR.requestdb.FurnitureRequest;
+import edu.wpi.teamR.requestdb.MealRequest;
+import edu.wpi.teamR.requestdb.RequestStatus;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import org.controlsfx.control.SearchableComboBox;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class MealDeliveryController {
 
@@ -20,6 +28,63 @@ public class MealDeliveryController {
 
     @FXML public void initialize() {
         cancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+        resetButton.setOnMouseClicked(event -> clear());
+        submitButton.setOnMouseClicked(event -> submit());
+
+        mealTypeField.setValue("Select Furniture");
+        locationField.setValue("Select Your Location");
     }
 
+    public Timestamp CurrentDateTime(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        return new Timestamp(now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now.getHour(),now.getMinute(),now.getSecond(),now.getNano());
+    }
+
+    @FXML
+    public void clear() {
+        nameField.clear();
+        staffMemberField.clear();
+        notesField.clear();
+
+        mealTypeField.setValue("Select Meal");
+        locationField.setValue("Select Your Location");
+    }
+
+    @FXML
+    public void submit() {
+        String mealType = mealTypeField.getValue().toString();
+        String location = locationField.getValue().toString();
+
+        if (mealType == "Select Meal") {
+            mealType = "";
+        }
+
+        if (location == "Select Your Location") {
+            location = "";
+        }
+
+        Navigation.navigate(Screen.HOME);
+        int id = 0;
+        ArrayList<MealRequest> foodList = MealRequestDAO.getInstance().getFoodRequests();
+        for(MealRequest foodRequest: foodList){
+            if(id < foodRequest.getRequestID()){
+                id = foodRequest.getRequestID();
+            }
+        }
+        ArrayList<FurnitureRequest> furnList = FurnitureRequestDAO.getInstance().getFurnitureRequests();
+        for(FurnitureRequest furnitureRequest: furnList){
+            if(id < furnitureRequest.getRequestID()){
+                id = furnitureRequest.getRequestID();
+            }
+        }
+        id++;
+        try {
+            MealRequestDAO.getInstance().addFoodRequest(id, nameField.getText(), location, mealType, staffMemberField.getText(), notesField.getText(), CurrentDateTime(), RequestStatus.Unstarted);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
