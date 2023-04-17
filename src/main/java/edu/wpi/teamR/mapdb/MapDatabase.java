d690fa21ec24fc56d82462ce1d0a650d2141f160
altonlm <121144911+altonlm@users.noreply.github.com>
2023-04-16 21:09:07 -0400

    Fixed error (fingers crossed)

--- src/main/java/edu/wpi/teamR/mapdb/MapDatabase.java
+++ src/main/java/edu/wpi/teamR/mapdb/MapDatabase.java
@@ -8,6 +8,8 @@ import edu.wpi.teamR.csv.CSVReader;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Objects;
 
 import static edu.wpi.teamR.mapdb.NodeDAO.parseNodes;
 
@@ -183,7 +185,7 @@ public class MapDatabase {
         ResultSet resultSet = preparedStatement.executeQuery();
 
         ArrayList<MapLocation> mapLocations = new ArrayList<>();
-        Node lastNode = new Node(-1, 0, 0, "", ""); //need to initialize so that it doesm't error
+        Node lastNode = new Node(-100, 0, 0, "", "");
         Node currentNode;
         ArrayList<LocationName> locationNames = new ArrayList<>();
         LocationName locationName;
@@ -201,16 +203,15 @@ public class MapDatabase {
 
             boolean noLocationNameForNode = longName==null && shortName==null && nodeType==null;
             boolean continuingLastNode = lastNode.getNodeID()==nodeID;
-            if (continuingLastNode){
-                if (!noLocationNameForNode) {
+            boolean noLastNode = lastNode.getNodeID()==-100;
+            if (continuingLastNode&&!noLocationNameForNode){
+                locationNames.add(locationName);
+            } else if (!continuingLastNode){
+                if (!noLastNode)
+                    mapLocations.add(new MapLocation(lastNode, locationNames));
+                locationNames = new ArrayList<LocationName>();
+                if (!noLocationNameForNode)
                     locationNames.add(locationName);
-                }
-            } else {
-                mapLocations.add(new MapLocation(lastNode, locationNames)); //if you've reached the end of the list of locations then you're ready to add it
-                locationNames = new ArrayList<>(); //reset list for the next node
-                if (!noLocationNameForNode) {
-                    locationNames.add(locationName); //add first entry for the next node
-                }
             }
 
             lastNode = currentNode;
