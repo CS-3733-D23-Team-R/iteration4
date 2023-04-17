39f9167dfe68b6801adb7e435d4844eec34ad4d9
SimonAJScott <simon.aj.scott@gmail.com>
2023-04-17 16:55:00 -0400

    progress

--- src/main/java/edu/wpi/teamR/controllers/AdminProfilePageController.java
+++ src/main/java/edu/wpi/teamR/controllers/AdminProfilePageController.java
@@ -1,4 +1,21 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.navigation.Navigation;
+import edu.wpi.teamR.navigation.Screen;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class AdminProfilePageController {
+    @FXML Text name, email, occupation, DateOfJoining, phone, time;
+    @FXML VBox thread;
+    ImageView ProfilePicture;
+    @FXML Button ToMapEditor, ToEmployeeManager;
+    @FXML
+    public void initialize(){
+        ToMapEditor.setOnAction(event -> Navigation.navigate(Screen.MAP_EDITOR));
+        
+    }
 }
--- src/main/java/edu/wpi/teamR/controllers/StaffProfilePageController.java
+++ src/main/java/edu/wpi/teamR/controllers/StaffProfilePageController.java
@@ -1,4 +1,17 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.requestdb.RequestDatabase;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class StaffProfilePageController {
+    @FXML Text name, email, occupation, DateOfJoining, phone, time;
+    @FXML VBox monday, tuesday, wednesday, thursday, friday, saturday, sunday, thread;
+
+    ImageView CreateNewMessage;
+
+    public void initialize(){
+    }
 }
