6da34b6645f94cb35b6535ff21fd6186a3600960
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-04-17 19:30:42 -0400

    Update Signage Configuration Page and made the DirectionArrows methods in MapDatabase public

--- src/main/java/edu/wpi/teamR/controllers/SignageConfigurationController.java
+++ src/main/java/edu/wpi/teamR/controllers/SignageConfigurationController.java
@@ -1,12 +1,19 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.mapdb.Direction;
+import edu.wpi.teamR.mapdb.DirectionArrow;
+import edu.wpi.teamR.mapdb.DirectionArrowDAO;
+import edu.wpi.teamR.mapdb.MapDatabase;
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class SignageConfigurationController {
     public void start(Stage primaryStage) {
@@ -14,14 +21,24 @@ public class SignageConfigurationController {
     }
 
     @FXML Button backButton;
-    @FXML TableView configurationTable;
-    @FXML TableColumn directionColumn;
-    @FXML TableColumn locationColumn;
-    @FXML TableColumn iconColumn;
-    @FXML TableColumn saveColumn;
+    @FXML TableView<DirectionArrow> configurationTable;
+    @FXML TableColumn<DirectionArrow, Direction> directionColumn;
+    @FXML TableColumn<DirectionArrow, String> locationColumn;
+    @FXML TableColumn<DirectionArrow, Integer> iconColumn;
+    @FXML TableColumn<DirectionArrow, Void> saveColumn;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException, ClassNotFoundException {
+        MapDatabase aMapDatabase = new MapDatabase();
+
+        iconColumn.setCellValueFactory(new PropertyValueFactory<>("kioskID"));
+        directionColumn.setCellValueFactory(new PropertyValueFactory<>("direction"));
+        locationColumn.setCellValueFactory(new PropertyValueFactory<>("longname"));
+
+        for(DirectionArrow directionArrow : aMapDatabase.getDirectionArrows()){
+            configurationTable.getItems().add(directionArrow);
+        }
+
         backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
     }
 }
