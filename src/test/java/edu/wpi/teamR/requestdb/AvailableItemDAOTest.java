634f7a21bd3962f26d8cc3895da9da4baff23a3b
AshKitturGitHub <ashtonkittur@gmail.com>
2023-04-18 19:37:34 -0400

    Altered some code based on Alton's direction

--- src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
+++ src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
@@ -15,16 +15,10 @@ public class AvailableItemDAOTest {
     private static AvailableItemDAO availableItemDAO;
     @Test
     void getAvailableItemsByTypeWithinRangeSortedByPrice() throws SQLException, ClassNotFoundException {
-        // create two AvailableItem(s) with appropriate differing data-types
-        availableItemDAO.getAvailableItemsByTypeWithinRangeSortedByPrice(RequestType.Flower, 0.1,0.1, SortOrder.unsorted); // execute said function with appropriate parameter
-        // check that the function retrieved the correct AvailableItem through multiple assertEquals
     }
 
     @Test
     void getAvailableItemByName() throws SQLException, ClassNotFoundException, ItemNotFoundException {
-        // create two AvailableItem(s) with different names
-        availableItemDAO.getAvailableItemByName("",RequestType.Flower); // execute said function with appropriate parameter
-        // check that the function retrieved the correct AvailableItem through multiple assertEquals
     }
 
 }
