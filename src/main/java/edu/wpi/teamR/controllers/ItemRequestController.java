3dee6dce08d3450c52836047eb94764a72c3f5cf
john-robinson-git <john.robinson.engineering@gmail.com>
2023-04-18 03:12:03 -0400

    fixed shopping cart and created basic functionality in ItemRequestController

--- src/main/java/edu/wpi/teamR/controllers/ItemRequestController.java
+++ src/main/java/edu/wpi/teamR/controllers/ItemRequestController.java
@@ -1,16 +1,22 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.Main;
 import edu.wpi.teamR.datahandling.ShoppingCart;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import edu.wpi.teamR.requestdb.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
+
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class ItemRequestController {
 
@@ -24,7 +30,20 @@ public class ItemRequestController {
     private TextField itemMinField;
 
     @FXML
-    private TableView<?> itemTable;
+    private TableView<AvailableItem> itemTable;
+
+    @FXML
+    private TableColumn<AvailableItem, String> itemNameColumn;
+
+    @FXML
+    private TableColumn<AvailableItem, String> itemDetailsColumn;
+
+    @FXML
+    private TableColumn<AvailableItem, Double> itemPriceColumn;
+    @FXML
+    private TableColumn<AvailableItem, Void> itemQuantityColumn;
+    @FXML
+    private TableColumn<AvailableItem, Void> itemAddToCartColumn;
 
     @FXML
     private Text itemTentoTwenty;
@@ -59,13 +78,69 @@ public class ItemRequestController {
         itemTentoTwenty.setOnMouseExited(event -> textStopHover(itemTentoTwenty));
 
         itemTwentytoThirty.setOnMouseEntered(event -> textHover(itemTwentytoThirty));
-        itemTwentytoThirty.setOnMouseExited(event -> textStopHover(itemTentoTwenty));
+        itemTwentytoThirty.setOnMouseExited(event -> textStopHover(itemTwentytoThirty));
 
         itemThirtytoForty.setOnMouseEntered(event -> textHover(itemThirtytoForty));
         itemThirtytoForty.setOnMouseExited(event -> textStopHover(itemThirtytoForty));
 
         itemThirtyPlus.setOnMouseEntered(event -> textHover(itemThirtyPlus));
         itemThirtyPlus.setOnMouseExited(event -> textStopHover(itemThirtyPlus));
+
+        itemNameColumn.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        itemPriceColumn.setCellValueFactory(new PropertyValueFactory<>("itemPrice"));
+        itemDetailsColumn.setCellValueFactory(new PropertyValueFactory<>("imageReference"));
+        addButtonToTable();
+
+        try {
+            for (AvailableItem item : RequestDatabase.getInstance().getAvailableItemsByTypeWithinRangeSortedByPrice(RequestType.Meal, null, null, SortOrder.unsorted)) {
+                itemTable.getItems().add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void addButtonToTable() {
+        Callback<TableColumn<AvailableItem, Void>, TableCell<AvailableItem, Void>> cellFactory = new Callback<TableColumn<AvailableItem, Void>, TableCell<AvailableItem, Void>>() {
+            @Override
+            public TableCell<AvailableItem, Void> call(final TableColumn<AvailableItem, Void> param) {
+                return new TableCell<AvailableItem, Void>() {
+
+                    private final Button btn = new Button();
+                    {
+                        ImageView imageView = new ImageView(Objects.requireNonNull(Main.class.getResource("images/delete.png")).toExternalForm());
+                        imageView.setPreserveRatio(true);
+                        imageView.setFitHeight(30);
+                        btn.getStyleClass().add("food_furniture-clear-button");
+                        btn.setGraphic(imageView);
+                        btn.setOnAction((ActionEvent event) -> {
+                            AvailableItem data = getTableView().getItems().get(getIndex());
+                            itemTable.getItems().remove(data);
+                            try {
+                                ShoppingCart.getInstance().addItem(data, 1);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+            }
+        };
+
+        itemAddToCartColumn.setCellFactory(cellFactory);
     }
 
     private void textHover(Text text){
@@ -73,6 +148,6 @@ public class ItemRequestController {
     }
 
     private void textStopHover(Text text){
-        text.setFill(Color.RED);
+        text.setFill(Color.valueOf("#BDBDBD"));
     }
 }
