03190d111c5e25cc09785b76e09ac72a4ac49d2a
AshKitturGitHub <ashtonkittur@gmail.com>
2023-04-18 18:51:06 -0400

    Wrote some code for AvailableItemDAOTest

--- src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
+++ src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
@@ -1,14 +1,30 @@
 package edu.wpi.teamR.requestdb;
 
 import edu.wpi.teamR.Configuration;
+import edu.wpi.teamR.ItemNotFoundException;
 import edu.wpi.teamR.mapdb.MapDatabase;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class AvailableItemDAOTest {
 
+    private static AvailableItemDAO availableItemDAO;
+    @Test
+    void getAvailableItemsByTypeWithinRangeSortedByPrice() throws SQLException, ClassNotFoundException {
+        // create two AvailableItem(s) with appropriate differing data-types
+        availableItemDAO.getAvailableItemsByTypeWithinRangeSortedByPrice(RequestType.Flower, 0.1,0.1, SortOrder.unsorted); // execute said function with appropriate parameter
+        // check that the function retrieved the correct AvailableItem through multiple assertEquals
+    }
+
+    @Test
+    void getAvailableItemByName() throws SQLException, ClassNotFoundException, ItemNotFoundException {
+        // create two AvailableItem(s) with different names
+        availableItemDAO.getAvailableItemByName("",RequestType.Flower); // execute said function with appropriate parameter
+        // check that the function retrieved the correct AvailableItem through multiple assertEquals
+    }
 
 }
