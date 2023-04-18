c03eba5a6fb5b61d34130e4cec79091a1e992b43
john-robinson-git <john.robinson.engineering@gmail.com>
2023-04-18 01:27:19 -0400

    introduced new itemRequestPage

--- src/main/java/edu/wpi/teamR/controllers/ItemRequestController.java
+++ src/main/java/edu/wpi/teamR/controllers/ItemRequestController.java
@@ -6,25 +6,73 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import edu.wpi.teamR.requestdb.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class ItemRequestController {
 
-    private ShoppingCart cart;
-
+    @FXML
+    private ComboBox<String> itemFilterByComboBox;
 
     @FXML
-    MFXComboBox requestTypeBox;
+    private TextField itemMaxField;
 
-    public ShoppingCart getShoppingCartInstance(){
-        return cart;
-    }
+    @FXML
+    private TextField itemMinField;
+
+    @FXML
+    private TableView<?> itemTable;
+
+    @FXML
+    private Text itemTentoTwenty;
+
+    @FXML
+    private Text itemThirtytoForty;
+
+    @FXML
+    private Text itemTwentytoThirty;
+
+    @FXML
+    private MFXComboBox<RequestType> itemTypeBox;
+
+    @FXML
+    private Text itemZerotoTen;
+
+    @FXML
+    private Text itemThirtyPlus;
+
 
     @FXML public void initialize(){
-        this.cart = cart;
-        ObservableList<RequestType> requestTypeList = FXCollections.observableArrayList();
-        requestTypeList.add(RequestType.flower);
+        ObservableList<RequestType> itemTypeList = FXCollections.observableArrayList();
+        for (RequestType type : RequestType.values()) {itemTypeList.add(type);} //add all request types to combo box
+        itemTypeBox.setItems(itemTypeList);
 
+        itemFilterByComboBox.setItems(FXCollections.observableArrayList("Unsorted", "Price: Low to High", "Price: High to Low"));
 
-        requestTypeBox.setItems();
+        itemZerotoTen.setOnMouseEntered(event -> textHover(itemZerotoTen));
+        itemZerotoTen.setOnMouseExited(event -> textStopHover(itemZerotoTen));
+
+        itemTentoTwenty.setOnMouseEntered(event -> textHover(itemTentoTwenty));
+        itemTentoTwenty.setOnMouseExited(event -> textStopHover(itemTentoTwenty));
+
+        itemTwentytoThirty.setOnMouseEntered(event -> textHover(itemTwentytoThirty));
+        itemTwentytoThirty.setOnMouseExited(event -> textStopHover(itemTentoTwenty));
+
+        itemThirtytoForty.setOnMouseEntered(event -> textHover(itemThirtytoForty));
+        itemThirtytoForty.setOnMouseExited(event -> textStopHover(itemThirtytoForty));
+
+        itemThirtyPlus.setOnMouseEntered(event -> textHover(itemThirtyPlus));
+        itemThirtyPlus.setOnMouseExited(event -> textStopHover(itemThirtyPlus));
+    }
+
+    private void textHover(Text text){
+        text.setFill(Color.WHITE);
+    }
+
+    private void textStopHover(Text text){
+        text.setFill(Color.RED);
     }
 }
