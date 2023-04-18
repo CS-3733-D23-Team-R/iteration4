b3f3dcdb4b7e5588072f6b64a8af55c9cb4b7280
AshKitturGitHub <ashtonkittur@gmail.com>
2023-04-18 18:37:11 -0400

    Renamed AvailableItemRequestDAOTest to AvailableItemDAOTest for consistency, some starterCode (Alton may have other ideas?)

--- src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
+++ src/test/java/edu/wpi/teamR/requestdb/AvailableItemDAOTest.java
@@ -9,17 +9,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class AvailableItemDAOTest {
-    private static AvailableItemDAO availableItemDAO;
-    private static Connection connection;
 
-    @BeforeAll
-    static void starterFunction() throws SQLException, ClassNotFoundException {
-        Configuration.changeSchemaName("iteration2test");
-        connection = Configuration.getConnection();
-        availableItemDAO = new AvailableItemDAO();
-    }
-    @BeforeEach
-    void deleteOldData() throws SQLException {
-       // availableItemDAO.deleteAllAvailableItemDAO(); // ?
-    }
+
 }
