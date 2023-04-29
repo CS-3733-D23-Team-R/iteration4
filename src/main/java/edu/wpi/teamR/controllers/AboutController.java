ab933a6f41f151f3b6678b9fc60d47a062bf3374
SimonAJScott <simon.aj.scott@gmail.com>
2023-04-29 18:21:54 -0400

    progress

--- src/main/java/edu/wpi/teamR/controllers/AboutController.java
+++ src/main/java/edu/wpi/teamR/controllers/AboutController.java
@@ -1,4 +1,32 @@
 package edu.wpi.teamR.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class AboutController {
+    @FXML VBox
+            benFront, benBack,
+            altonFront, altonBack,
+            nathanielFront, nathanielBack,
+            bryceFront, bryceBack,
+            johnFront, johnBack,
+            gwenFront, gwenBack,
+            zeshFront, zeshBack,
+            simonFront, simonBack,
+            ashFront, ashBack,
+            jpFront, jpBack;
+    @FXML
+    public void initialize(){
+        benBack.setVisible(false);
+        altonBack.setVisible(false);
+        nathanielBack.setVisible(false);
+        bryceBack.setVisible(false);
+        johnBack.setVisible(false);
+        gwenBack.setVisible(false);
+        zeshBack.setVisible(false);
+        simonBack.setVisible(false);
+        ashBack.setVisible(false);
+        ashBack.setVisible(false);
+        johnBack.setVisible(false);
+    }
 }
