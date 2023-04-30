c9e82ccba89cbec2cc8eda83ec3e1ea0a3e9bf4e
jp3wi <jpwilusz@wpi.edu>
2023-04-30 18:57:43 -0400

    Finished structure of MovePatient

--- src/main/java/edu/wpi/teamR/controllers/movePatientController.java
+++ src/main/java/edu/wpi/teamR/controllers/movePatientController.java
@@ -1,4 +1,18 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.datahandling.MapStorage;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import net.kurobako.gesturefx.GesturePane;
+
+import javax.swing.text.html.ImageView;
+
 public class movePatientController {
+    @FXML GesturePane miniMap;
+    AnchorPane mapPane;
+
+    public void initialize() {
+        mapPane.getChildren().add(MapStorage.getFirstFloor());
+        miniMap.setContent(mapPane);
+    }
 }
