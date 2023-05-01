53ceb133d3eb9e17e0f12360ef49dbd9f8db4fc6
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-05-01 16:41:54 -0400

    Added a button to the help page, that when clicked will navigate to our user manual

--- src/main/java/edu/wpi/teamR/controllers/HelpController.java
+++ src/main/java/edu/wpi/teamR/controllers/HelpController.java
@@ -1,19 +1,41 @@
 package edu.wpi.teamR.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class HelpController {
 
+  @FXML Button userManualButton;
+
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    userManualButton.setOnMouseClicked(event -> downloadUserManual());
+  }
 
   @FXML
   public void close(Stage primaryStage) {
     primaryStage.close();
   }
 
-  @FXML public void start(Stage primaryStage) {
+  @FXML
+  public void start(Stage primaryStage) {
     primaryStage.show();
+  }
+
+  private void downloadUserManual() {
+    String url = "https://docs.google.com/document/d/1v4bLPNDgx1ff8fVih4D98vuN3wGwO7FgsX2yB5DXQvU/edit?usp=sharing";
+
+    try {
+      Desktop.getDesktop().browse(new URI(url));
+    } catch (IOException | URISyntaxException e) {
+      e.printStackTrace();
+    }
   }
 }
