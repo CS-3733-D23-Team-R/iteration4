4767bf89de7efa2d0029817fec47d1df4fdfee8d
jp3wi <jpwilusz@wpi.edu>
2023-05-01 17:11:40 -0400

    Implemented functionality upon selecting patient

--- src/main/java/edu/wpi/teamR/controllers/movePatientController.java
+++ src/main/java/edu/wpi/teamR/controllers/movePatientController.java
@@ -1,7 +1,11 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.ItemNotFoundException;
 import edu.wpi.teamR.datahandling.MapStorage;
+import edu.wpi.teamR.login.User;
+import edu.wpi.teamR.login.UserDatabase;
 import edu.wpi.teamR.requestdb.Patient;
+import edu.wpi.teamR.requestdb.PatientMove;
 import edu.wpi.teamR.requestdb.RequestDatabase;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
@@ -10,41 +14,76 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import net.kurobako.gesturefx.GesturePane;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class movePatientController {
 
     RequestDatabase db;
+    UserDatabase udb;
+    ArrayList<String> rooms = new ArrayList<>();
+    ObservableList<String> patientList, roomList, staffList;
     @FXML GesturePane miniMap;
     AnchorPane mapPane;
     @FXML MFXComboBox patientName, patientDestination, staffMember;
     @FXML MFXTextField currentLocation;
     @FXML MFXDatePicker moveDate;
+    @FXML TableView<PatientMove> moveTable;
+    @FXML TableColumn<PatientMove, String> locCol, staffCol;
+    @FXML TableColumn<PatientMove, Integer> idCol;
+    @FXML TableColumn<PatientMove, Timestamp> dateCol;
 
-    public void initialize() throws SQLException {
+    public void initialize() throws SQLException, ItemNotFoundException {
         db = new RequestDatabase();
+        udb = new UserDatabase();
         mapPane.getChildren().add(MapStorage.getFirstFloor());
         miniMap.setContent(mapPane);
 
         ArrayList<Patient> pList = db.getPatients();
-        ArrayList<String> pListDisplay = new ArrayList<>();
+        ArrayList<String> pList0 = new ArrayList<>();
         for(Patient p : pList)
-            pListDisplay.add(p.getPatientName() + " - ID " + p.getPatientID());
-        ObservableList<String> patientList = FXCollections.observableArrayList(pListDisplay);
+            pList0.add(p.getPatientName() + " - ID: " + p.getPatientID());
+        patientList = FXCollections.observableArrayList(pList0);
         patientName.setItems(patientList);
 
-        // For patientDestination, use Map Nodes(?)
+        for(int i = 0; i < 25; i++)
+            rooms.add("Room " + (i + 1));
+        roomList = FXCollections.observableArrayList(rooms);
+        patientDestination.setItems(roomList);
 
-
-        // For staffMember, use list of staff accounts
+        ArrayList<User> uList = udb.getUsers();
+        ArrayList<String> userList0 = new ArrayList<>();
+        for(User u : uList)
+            userList0.add(u.getName() + " - Username: " + u.getStaffUsername());
+        staffList = FXCollections.observableArrayList(userList0);
+        staffMember.setItems(staffList);
 
         Platform.runLater(() -> moveDate.setValue(LocalDate.now()));
 
+        idCol.setCellValueFactory(new PropertyValueFactory<>("patientID"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+        locCol.setCellValueFactory(new PropertyValueFactory<>("longName"));
+        staffCol.setCellValueFactory(new PropertyValueFactory<>("staffUsername"));
 
+        patientName.setOnAction(event -> {
+            try {
+                String[] patientSelection = patientName.getValue().toString().split(" ");
+                String patientID = patientSelection[patientSelection.length - 1];
+                ArrayList<PatientMove> selection = db.getPatientMovesByPatient(Integer.parseInt(patientID));
+                moveTable.setItems(FXCollections.observableArrayList(selection));
+                PatientMove currentMove = db.getCurrentPatientMove(Integer.parseInt(patientID));
+                currentLocation.setText(currentMove.getLongName());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
