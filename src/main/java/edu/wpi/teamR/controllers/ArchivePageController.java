2548089b29db71e079fb8f2cf3657b1ef3fb9864
TurtleCat02 <bcspenguin1@gmail.com>
2023-04-30 21:33:56 -0400

    Updated archive popup

--- src/main/java/edu/wpi/teamR/controllers/ArchivePageController.java
+++ src/main/java/edu/wpi/teamR/controllers/ArchivePageController.java
@@ -5,58 +5,61 @@ import edu.wpi.teamR.archive.Archiver;
 import edu.wpi.teamR.archive.CSVParameterException;
 import edu.wpi.teamR.login.UserDatabase;
 import edu.wpi.teamR.mapdb.MapDatabase;
-import edu.wpi.teamR.navigation.Navigation;
-import edu.wpi.teamR.navigation.Screen;
 import edu.wpi.teamR.requestdb.RequestDatabase;
-import io.github.palexdev.materialfx.controls.MFXButton;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 public class ArchivePageController {
 
-    @FXML
-    Button fileButton;
-    @FXML Button pathButton;
+    @FXML Button chooseImportButton;
     @FXML Button submitImport;
     @FXML Button submitExport;
-    @FXML Text fileText;
-    @FXML Text pathText;
+    @FXML Text importText;
+    @FXML Text exportText;
 
     static File selectedFile;
     static File selectedDirectory;
     FileChooser fileChooser;
-
-    DirectoryChooser directoryChooser;
 
     MapDatabase mapdb = App.getMapData().getMapdb();
 
     Archiver archiver = new Archiver(mapdb, new UserDatabase(), new RequestDatabase());
 
     @FXML
-    public void initialize() throws SQLException, ClassNotFoundException {
-        fileButton.setOnAction(event -> openFile());
+    public void initialize() {
+        chooseImportButton.setOnAction(event -> openFile());
         submitImport.setOnMouseClicked(event -> {
             try {
                 submit();
-            } catch (SQLException | ClassNotFoundException | FileNotFoundException | CSVParameterException e) {
-                fileText.setText("Error: Try Again");
-                throw new RuntimeException(e);
+                importText.setText("Successfully imported backup.");
+            } catch (SQLException | CSVParameterException | IOException e) {
+                if (e.getClass() == SQLException.class) {
+                    importText.setText("Error uploading to database.");
+                } else if (e.getCause() instanceof CSVParameterException) {
+                    importText.setText("Error parsing CSV parameters.");
+                } else if (e.getCause() instanceof IOException) {
+                    importText.setText("Error reading file.");
+                }
+                e.printStackTrace();
             }
         });
-        pathButton.setOnMouseClicked(event -> chooseDirectory());
         submitExport.setOnMouseClicked(event -> {
             try {
                 export();
-            } catch (ClassNotFoundException | SQLException | IOException e) {
-                pathText.setText("Error: Try Again");
+                exportText.setText("Successfully exported database.");
+            } catch (SQLException | IOException e) {
+                if (e.getClass() == SQLException.class) {
+                    exportText.setText("Error reading from database.");
+                } else if (e.getClass() == IOException.class) {
+                    exportText.setText("Error reading file.");
+                }
                 e.printStackTrace();
             }
         });
@@ -64,28 +67,22 @@ public class ArchivePageController {
 
     public void openFile() {
         fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("./backups/"));
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Zip Files", "*.zip"));
         fileChooser.setTitle("Open File");
-        selectedFile = fileChooser.showOpenDialog(fileButton.getScene().getWindow());
-        fileText.setText(selectedFile.getName());
+        selectedFile = fileChooser.showOpenDialog(chooseImportButton.getScene().getWindow());
+        importText.setText(selectedFile.getName());
     }
 
-    public void submit() throws SQLException, FileNotFoundException, ClassNotFoundException, CSVParameterException {
+    public void submit() throws SQLException, IOException, CSVParameterException {
         archiver.restoreArchive(selectedFile.getAbsolutePath());
         selectedFile = null;
-        fileText.setText("");
+        importText.setText("");
     }
 
-    public void chooseDirectory() {
-        directoryChooser = new DirectoryChooser();
-        directoryChooser.setTitle("Choose Directory");
-        selectedDirectory = directoryChooser.showDialog(pathButton.getScene().getWindow());
-        pathText.setText(selectedDirectory.getAbsolutePath());
-    }
-
-    public void export() throws SQLException, IOException, ClassNotFoundException {
-        archiver.createArchive(selectedDirectory.getAbsolutePath() + "/archive.zip");
+    public void export() throws SQLException, IOException {
+        String exportFile = archiver.createArchive();
         selectedDirectory = null;
-        pathText.setText("");
+        exportText.setText("Created backup " + exportFile);
     }
 }
